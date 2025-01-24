82052ce246d1e292e3f12a1cd5b0f1e591505c3d
el-amine-404 <131032600+el-amine-404@users.noreply.github.com>
2025-01-24 10:48:56 +0100

    add gateway-service hello project

---
+++ gateway-service/src/test/java/com/amine/gateway_service/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amine.gateway_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
