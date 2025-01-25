8273c693b6958a6a98f476080e390efc55b7268c
el-amine-404 <131032600+el-amine-404@users.noreply.github.com>
2025-01-25 15:30:42 +0100

    discovery-registry-server: configure the server

--- discovery-registry-server/src/main/java/com/amine/discovery_registry_server/DiscoveryRegistryServerApplication.java
+++ discovery-registry-server/src/main/java/com/amine/discovery_registry_server/DiscoveryRegistryServerApplication.java
@@ -2,8 +2,10 @@ package com.amine.discovery_registry_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class DiscoveryRegistryServerApplication {
 
 	public static void main(String[] args) {
