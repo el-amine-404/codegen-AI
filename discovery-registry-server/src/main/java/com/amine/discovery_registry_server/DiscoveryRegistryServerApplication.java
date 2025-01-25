8bac6f436262481426777a53c1cb1984133d89df
el-amine-404 <131032600+el-amine-404@users.noreply.github.com>
2025-01-25 15:17:33 +0100

    add discovery-registry-server/

---
+++ discovery-registry-server/src/main/java/com/amine/discovery_registry_server/DiscoveryRegistryServerApplication.java
@@ -0,0 +1,13 @@
+package com.amine.discovery_registry_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoveryRegistryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryRegistryServerApplication.class, args);
+	}
+
+}
